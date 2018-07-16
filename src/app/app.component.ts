fd545c7c44116686a163fb2c2ed139b7b914802a
serkan ertan <s.ertan@lmu.de>
2018-07-16 17:22:17 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '../../node_modules/angularfire2/firestore';
+import { Observable } from '../../node_modules/rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,86 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  isUpdate = false;
+  model: Employee = new Employee();
+
+  @ViewChild('dynamicBtn') dynamicBtn: ElementRef;
+  displayForm = false;
+  employees: Observable<Employee[]>;
+  employeeDoc: AngularFirestoreDocument<Employee>;
+  employeesDoc: AngularFirestoreCollection<Employee>;
+  employee: Observable<Employee>;
+
+  constructor(private db: AngularFirestore) {
+    this.employeesDoc = db.collection('employees');
+    this.employees = this.employeesDoc.valueChanges();
+
+    // this.itemDoc = db.doc<Employee>('employees/D.Uysal');
+    // this.item = this.itemDoc.valueChanges();
+  }
+
+  Edit(employee: Employee) {
+    console.log(employee);
+    this.model = employee;
+    this.displayForm = true;
+    this.isUpdate = true;
+  }
+
+  Delete(employee: Employee) {
+    console.log(employee);
+  }
+
+  DynamicbtnClikc() {
+    if (this.dynamicBtn.nativeElement.innerText === "Save") {
+      const docName = `${this.model.fName.charAt(0)}.${this.model.lName}`;
+      this.employeesDoc.doc(docName).set(Object.assign({}, this.model), {
+        merge: this.isUpdate
+      }).then(x => {
+        alert('Success');
+      }).catch(err =>  {
+        console.log(err);
+        alert('Not success');
+      });
+    }
+  }
+
+  CreateEmployee() {
+    console.log("CreateEmployee");
+    this.displayForm = !this.displayForm;
+    this.isUpdate = false;
+    // this.dynamicBtn.nativeElement.text('Save Changes');
+  }
+
+  addItem(employee: Employee) {
+    this.employeesDoc.add(employee);
+  }
+
+  addItem2(name: string) {
+    const id = this.db.createId();
+    const employee: IEmployee = { id, fName: name };
+    this.employeesDoc.doc(id).set(employee);
+  }
+  update(employee: Employee) {
+    this.employeeDoc.update(employee);
+  }
+
+  filter(name: string) {
+    this.db.collection('employees', x => x.where('fName', '==',  name));
+  }
+}
+
+interface IEmployee {
+  id: string;
+  fName: string;
+}
+
+class Employee {
+  fName: string;
+  lName: string;
+  email: string;
+  age: number;
+  gender: string;
+  yearsOfExperience: number;
+  isFullTime: boolean;
 }
